12360cac45412ad12f29b5fcbc598a61451b30e5
capac <capac@users.noreply.github.com>
2023-05-12 17:15:41 +0100

    Using a reduced data set of countries

--- life-expectancy-vs-gdp.py
+++ life-expectancy-vs-gdp.py
@@ -2,9 +2,11 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from pathlib import Path
 
-# plt.style.use('personal-style.mplstyle')
+# plt.style.use('seaborn-v0_8-whitegrid')
 
 data_dir = Path.home() / 'Programming/data/life-expectancy-vs-gdp-pc/'
+python_work_dir = Path.home() / 'Programming/Python/'
+work_dir = python_work_dir / 'data-visualization/life-expectancy-vs-gdp-pc/'
 
 # life expectancy data
 le_data_dir = data_dir / 'IHME-GBD_2019_DATA-7e5aa45e-1'
@@ -33,26 +35,34 @@ merged_df = gdp_nat_yr_df.merge(le_nat_yr_df, how='inner', left_on='Country Name
 merged_df = merged_df[['Country Name', '2019', 'val']]
 merged_df.rename(columns={'val': 'Life Expectancy', '2019': 'GDP per capita'},
                  inplace=True)
-merged_df.replace({'Country Name': {'Korea, Rep.': 'South Korea'}}, inplace=True)
+merged_df.replace({'Country Name': {'Korea, Rep.': 'South Korea', }}, inplace=True)
 
-# print(merged_df['GDP per capita'].to_list())
-# print(merged_df['Life Expectancy'].to_list())
+merged_df.set_index('Country Name', inplace=True)
+countries_above = ['Australia', 'Canada', 'Belgium',
+                   'Luxembourg', 'Netherlands', 'New Zealand',]
+countries_below = ['Switzerland', 'Austria', 'Germany', 'Denmark',
+                   'Spain', 'Finland', 'France', 'United Kingdom',
+                   'Greece', 'Ireland', 'Italy', 'Japan',
+                   'South Korea', 'Norway', 'Portugal', 'Singapore',
+                   'Sweden', 'United States',]
 
-# life expectancy plot for 2019
+select_countries_df = merged_df[merged_df.index.isin(countries_below)]
+
+# life expectancy plot versus GDP per capita for 2019
 fig, ax = plt.subplots(figsize=(12, 8))
-ax.scatter(merged_df['GDP per capita'], merged_df['Life Expectancy'],
-           alpha=0.5, color='g', s=60)
+ax.scatter(select_countries_df['GDP per capita'],
+           select_countries_df['Life Expectancy'],
+           alpha=0.6, color='royalblue', s=120)
 ax.set_xlabel('GDP per capita (USD)', fontsize=11)
 ax.set_ylabel('Life expectancy (years)', fontsize=11)
 ax.set_title('Life expectancy per GDP per capita (2019)', fontsize=12)
 
-countries = merged_df['Country Name'].to_list()
-gdp_pc = merged_df['GDP per capita'].to_list()
-le = merged_df['Life Expectancy'].to_list()
+gdp_pc = merged_df['GDP per capita']
+le = merged_df['Life Expectancy']
 
-for txt, x, y in zip(countries, gdp_pc, le):
-    ax.annotate(txt, (x-500, y-0.3))
+for label in countries_below:
+    ax.annotate(label, (gdp_pc.loc[label]-400, le.loc[label]-0.3))
 
 fig.tight_layout()
 plt.grid(linestyle=':')
-plt.savefig('life_expectancy_vs_gdp_pc.png', bbox_inches='tight')
+plt.savefig(work_dir / 'plots/life_expectancy_vs_gdp.png', bbox_inches='tight')
